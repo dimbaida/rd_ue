634cf9f87e1904c09f3a1a0baa4568ed85ebbdda
Dmytro Baidachnyi <dimbaida@gmail.com>
2024-08-29 13:04:48 +0300

    added the round to two decimal digits to health value

--- lesson_13.cpp
+++ lesson_13.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
 
-void outputFramedHealth(int health)
+void outputFramedHealth(float health)
 {
+    health = std::round(health * 100.0) / 100.0;
+
+    std::ostringstream stream;
+    stream << std::fixed << std::setprecision(2) << health;
+    std::string health_text = stream.str();
+
     std::string text = "\u250c\u2500";
-    for (int i=0; i<std::to_string(health).length() + 8; i++)
+    for (int i = 0; i < health_text.length() + 8; i++)
         text += "\u2500";
 
     text = text + "\u2500\u2510\n"
-                + "\u2502" + " HEALTH: " + std::to_string(health) + " \u2502\n"
+                + "\u2502" + " HEALTH: " + health_text + " \u2502\n"
                 + "\u2514\u2500";
 
-    for (int i=0; i<std::to_string(health).length() + 8; i++)
+    for (int i = 0; i < health_text.length() + 8; i++)
         text += "\u2500";
     text += "\u2500\u2518";
     std::cout << "\033[32m" << text << "\033[0m" << std::endl; 
